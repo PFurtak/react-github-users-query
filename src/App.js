1da18c03aaa65b0a1cfd7ea95777aebcd16b56ea
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-01 11:23:50 -0400

    added search class component and api call, added in functions for returning data from api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import UserCard from './components/UserCard';
 import UserCardList from './components/UserCardList';
+import Search from './components/Search';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: []
+  };
+
+  searchUsers = async text => {
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}`
+    );
+
+    this.setState({ users: res.data.items });
+  };
+
   render() {
+    const { users } = this.state;
+
     return (
       <div className='App'>
         <h1>GitHub Users</h1>
+        <Search searchUsers={this.searchUsers} />
+        <UserCardList users={users} />
       </div>
     );
   }
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export const UserCard = () => {
-  return <div></div>;
+export const UserCard = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div>
+      <img src={avatar_url} alt='' className='' style={{ width: '60px' }} />
+      <a href={html_url}>
+        <h3>{login}</h3>
+      </a>
+      <div></div>
+    </div>
+  );
 };
+
+export default UserCard;
